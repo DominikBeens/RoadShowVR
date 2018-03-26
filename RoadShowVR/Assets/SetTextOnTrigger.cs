8d85f3312f3eea4560b0da2d0a3821175a4097c3
Dominik Beens <dominik.beens@hotmail.nl>
2018-03-26 15:32:03 +0200

    added FPSController for testing + updates level layout

---
+++ RoadShowVR/Assets/SetTextOnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTextOnTrigger : MonoBehaviour 
+{
+
+    [SerializeField]
+    private string roomDescription;
+    [SerializeField]
+    private Text textToChange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textToChange.text = roomDescription;
+        }
+    }
+}
