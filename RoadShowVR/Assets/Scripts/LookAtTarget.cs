69df5034b10b6602d8182c6be48ccc704bf69616
Dominik Beens <dominik.beens@hotmail.nl>
2018-04-05 21:27:45 +0200

    improved VRLaser, all closed doors can be opened/closed, interactable objects should display a world space ui, new reticle

---
+++ RoadShowVR/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour 
+{
+
+    [SerializeField]
+    private Transform target;
+
+    private void Awake()
+    {
+        if (target == null)
+        {
+            target = Camera.main.transform;
+        }
+    }
+
+    private void Update()
+    {
+        transform.LookAt(target);
+    }
+}
